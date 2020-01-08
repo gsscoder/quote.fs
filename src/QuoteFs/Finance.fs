e63bb638b47c982026f4afed130c7840e5681544
Giacomo Stelluti Scala <gsscoder@gmail.com>
2020-01-08 10:35:40 +0100

    Removed redundant brackets from Finance.fs

--- src/QuoteFs/Finance.fs
+++ src/QuoteFs/Finance.fs
@@ -14,26 +14,26 @@ module Finance =
             ".INX"   // S&P 500
         ]
 
-    let getStockQuoteAsync(symbol) =
+    let getStockQuoteAsync symbol =
         let url = sprintf "%s/api/v3/company/profile/%s" baseUrl symbol
         async {
             let! response = Http.AsyncRequestString url
             return Decode.fromString StockQuote.Decoder response
         } |> Async.StartAsTask
 
-    let getStockQuote(symbol) =
+    let getStockQuote symbol =
             getStockQuoteAsync(symbol)
             |> Async.AwaitTask
             |> Async.RunSynchronously
 
-    let getIndexAsync(ticker) =
+    let getIndexAsync ticker =
         let url = sprintf "%s/api/v3/majors-indexes/%s" baseUrl ticker
         async {
             let! response = Http.AsyncRequestString url
             return Decode.fromString Index.Decoder response
         } |> Async.StartAsTask
 
-    let getIndex(ticker) =
+    let getIndex ticker =
         getIndexAsync(ticker)
         |> Async.AwaitTask
         |> Async.RunSynchronously
