b57ca8ec74101f235c4762afab4e9ca463e6efcd
Giacomo Stelluti Scala <gsscoder@gmail.com>
2020-01-08 11:34:59 +0100

    Rearranged position of Query methods

--- src/QuoteFs/Query.fs
+++ src/QuoteFs/Query.fs
@@ -4,8 +4,8 @@ open QuoteFs.Finance
 
 [<AbstractClass;Sealed>]
 type Query =
-    static member GetStockQuoteAsync symbol = getStockQuoteAsync symbol
-    static member GetStockQuote symbol = getStockQuote symbol
+    static member MajorIndexes : seq<string> = majorIndexes |> Seq.map id
     static member GetIndexAsync ticker = getIndexAsync ticker
     static member GetIndex ticker = getIndex ticker
-    static member MajorIndexes : seq<string> = majorIndexes |> Seq.map id
+    static member GetStockQuoteAsync symbol = getStockQuoteAsync symbol
+    static member GetStockQuote symbol = getStockQuote symbol
