4609867df01b03ee02f143cdcb700fa4c082cf60
Giacomo Stelluti Scala <gsscoder@gmail.com>
2020-01-09 16:07:56 +0100

    Real time price managed by CLI app

--- src/QuoteFs.CLI/Program.fs
+++ src/QuoteFs.CLI/Program.fs
@@ -20,15 +20,34 @@ let printQuote(quote : StockQuote) =
     indent "Day Max" quote.DayMax
     indent "Web Site" quote.WebSite
 
+let printPrice symbol price =
+    item "Quote" symbol 0
+    indent "Real Time Price" price
+
 let printIndex(index : Index) =
     item "Index" (sprintf "%s %s" index.Ticker index.Name) 0
     indent "Price" index.Price
     indent "Change" index.Change
     true
 
+type infoType =
+    | Indexes
+    | Symbol
+    | RealTimePrice
+
 type options = {
-    [<Option('s', "symbol", HelpText = "Prints detail of a stock quote")>] symbol : string
-}
+    [<Option('s', "symbol", HelpText ="Prints detail of a stock quote", SetName="symbol")>]
+    symbol : string
+    [<Option('r', "realtime-price", HelpText ="Prints real time price a stock quote", SetName="realtime-price")>]
+    realTime : string
+} with member this.getInfoType = 
+                    if isNull this.symbol |> not
+                    then Symbol
+                    elif isNull this.realTime |> not
+                    then RealTimePrice
+                    else Indexes // no quote given, means makor indexes
+
+
 
 [<Literal>]
 let exitOK = 0
@@ -40,9 +59,23 @@ let main argv =
     let result = CommandLine.Parser.Default.ParseArguments argv
     match result  with
         | :? Parsed<options> as parsed -> 
-                match parsed.Value.symbol with
-                    // no quote give, let's print 5 main indexes
-                    | null ->
+                match parsed.Value.getInfoType with
+                    | RealTimePrice ->
+                        let symbol = parsed.Value.realTime
+                        let result = symbol |> getPrice
+                        match result with
+                        | Ok price ->
+                            printPrice symbol price
+                            exitOK
+                        | _ -> exitFail
+                    | Symbol ->
+                        let result = parsed.Value.symbol |> getStockQuote
+                        match result with
+                        | Ok quote ->
+                            printQuote quote
+                            exitOK
+                        | _ -> exitFail
+                    | _ ->
                         let results = majorIndexes |> Seq.map (fun index ->
                             let result = getIndex index
                             match result with
@@ -51,11 +84,4 @@ let main argv =
                             )
                         if results |> Seq.contains false then exitFail
                         else exitOK
-                    | _ ->
-                        let result = parsed.Value.symbol |> getStockQuote
-                        match result with
-                        | Ok quote ->
-                            printQuote quote
-                            exitOK
-                        | _ -> exitFail
         | _ -> exitFail
